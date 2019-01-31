9dc319d5ad581d6fb2fb8ae7936d3a5bfabdd3e1
Ricky Huang <huangricky@gmail.com>
2019-01-31 16:25:48 -0500

    looking for error that results in spans maximum stack error

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
-
 class App extends React.Component {
-    state = { images: [] }
+    state = { images: [] };
 
     onSearchSubmit = async term => {
         console.log("Access to the term entere in SearchBar.js:", term);
         const res = await unsplash.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            
+            params: { query: term }            
         });
         console.log("this this this:", this);
         this.setState({ images: res.data.results });
